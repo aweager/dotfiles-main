dd3f5eb325af0f2c916f1458262191f8e13e8ced
Alexander Eager <aw.eager@gmail.com>
2024-01-18 00:29:20 -0800

    Auto-push

--- global/nvim/lua/config/tree.lua
+++ global/nvim/lua/config/tree.lua
@@ -5,3 +5,4 @@
 require("nvim-tree").setup()
 
 vim.keymap.set("n", "<m-space>", ":NvimTreeToggle " .. vim.g.root_dir .. "<CR>")
+vim.keymap.set("t", "<m-space>", "<c-\\><c-n>:NvimTreeToggle " .. vim.g.root_dir .. "<CR>")
