d0c5faec616c6b40c6e25afe596807e13a3c6862
aeager <aeager@anduril.com>
2024-01-17 19:57:14 -0800

    Auto-push

--- global/zsh/basics.zsh
+++ global/zsh/basics.zsh
@@ -3,18 +3,5 @@
 setopt interactivecomments
 export LSCOLORS=Exfxcxdxbxegedabagacad
 
-path=("$DEFAULT_USER_HOME/.local/bin" $path)
-
-# vim mode
-zplug "jeffreytse/zsh-vi-mode"
-
-function zvm_config() {
-    ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
-    ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
-}
-
 # each terminal gets its own history
 unsetopt share_history
-
-# direnv
-eval "$(direnv hook zsh)"
---
+++ global/zsh/path.zsh
@@ -0,0 +1,5 @@
+() {
+
+    path=("$DEFAULT_USER_HOME/.local/bin" $path)
+
+} "${0:a:h}"
