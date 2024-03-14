d8cdb4b9f5e8ae99274a286c57ef35fa54027ecb
Alexander Eager <aw.eager@gmail.com>
2024-03-13 20:07:46 -0700

    ctrl_w mappings in terminal mode

--- global/nvim/lua/windows.lua
+++ global/nvim/lua/windows.lua
@@ -4,3 +4,29 @@ end, {
     silent = true,
     desc = "Close the quick list window (cclose)",
 })
+
+local localmode = require("localmode")
+local ctrl_w_mappings = {
+    "w",
+    "j",
+    "k",
+    "h",
+    "l",
+    "s",
+    "v",
+}
+
+for _, c in pairs(ctrl_w_mappings) do
+    local ctrl_c = "<c-" .. c .. ">"
+    local normal_command = '"normal \\<c-w>\\' .. ctrl_c .. '"'
+    vim.keymap.set("t", "<c-w>" .. ctrl_c, function()
+        localmode.execute_move("t", function()
+            vim.cmd.execute(normal_command)
+        end)
+    end)
+    vim.keymap.set("t", "<c-w>" .. c, function()
+        localmode.execute_move("t", function()
+            vim.cmd.execute(normal_command)
+        end)
+    end)
+end
