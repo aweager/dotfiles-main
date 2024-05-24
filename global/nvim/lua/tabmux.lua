a2e1e04bc9aa06a3c77da613dd2e222857c67707
Alex Eager <aweager@users.noreply.github.com>
2024-05-23 18:21:11 -0700

    Register vars to be saved instead of manually reimplementing

--- global/nvim/lua/tabmux.lua
+++ global/nvim/lua/tabmux.lua
@@ -1,6 +1,15 @@
-vim.g.num_pinned_tabs = 0
-
 local M = {}
+local augroup = vim.api.nvim_create_augroup("AweTabMux", {})
+
+vim.api.nvim_create_autocmd("TabLeave", {
+    group = augroup,
+    callback = function()
+        vim.g.last_tab = vim.api.nvim_get_current_tabpage()
+    end,
+})
+
+vim.g.num_pinned_tabs = 0
+vim.g.last_tab = 1
 
 function M.toggle_pin()
     local tab_mux_vars = vim.t.mux or {}
@@ -35,6 +44,20 @@ function M.move_right()
         vim.cmd.tabmove("+1")
     end
     vim.cmd.redrawtabline()
+end
+
+function M.close_tab()
+    if #vim.api.nvim_list_tabpages() == 1 then
+        vim.cmd.quitall()
+    else
+        local tabpage = vim.api.nvim_get_current_tabpage()
+        local tabnr = vim.api.nvim_tabpage_get_number(tabpage)
+        if vim.api.nvim_tabpage_is_valid(vim.g.last_tab) then
+            local last_tabnr = vim.api.nvim_tabpage_get_number(vim.g.last_tab)
+            vim.cmd.tabnext(last_tabnr)
+        end
+        vim.cmd.tabclose(tabnr)
+    end
 end
 
 return M
