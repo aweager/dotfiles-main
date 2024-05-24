5863509ecc9c89d2336499bc1bcb6cf0de2623c4
Alex Eager <aweager@users.noreply.github.com>
2024-05-23 18:21:12 -0700

    Switch from zplug to zinit

--- global/zsh/vi-mode.zsh
+++ global/zsh/vi-mode.zsh
@@ -1,7 +1,8 @@
 # vim mode
-zplug "jeffreytse/zsh-vi-mode"
 
 function zvm_config() {
     ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
     ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
 }
+
+zinit light jeffreytse/zsh-vi-mode
