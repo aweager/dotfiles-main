09f19f0d1742ef3dc75f2042d3e882232d17824a
Alex Eager <aweager@users.noreply.github.com>
2024-05-23 18:21:13 -0700

    force refresh status bar on winch

--- global/zsh/vi-mode.zsh
+++ global/zsh/vi-mode.zsh
@@ -3,6 +3,9 @@
 function zvm_config() {
     ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
     ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
+
+    bindkey -M viins "^j" menu-complete
+    bindkey -M viins "^k" reverse-menu-complete
 }
 
 zinit light jeffreytse/zsh-vi-mode
