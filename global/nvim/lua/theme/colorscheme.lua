296cd900137ebe07a16c206f6d769532571105ea
Alex Eager <aweager@users.noreply.github.com>
2024-05-23 18:21:14 -0700

    set term colors in colorscheme

--- global/nvim/lua/theme/colorscheme.lua
+++ global/nvim/lua/theme/colorscheme.lua
@@ -15,6 +15,31 @@ colorscheme.config = function()
     vim.api.nvim_set_hl(0, "NonText", themeconfig.listchars.non_text)
     vim.api.nvim_set_hl(0, "Whitespace", themeconfig.listchars.whitespace)
     vim.api.nvim_set_hl(0, "WinSeparator", themeconfig.windows.separator)
+
+    -- TODO: unify this list somehow with wezterm, which is where i got it from
+    local colors = {
+        "#000000",
+        "#cc5555",
+        "#55cc55",
+        "#cdcd55",
+        "#5455cb",
+        "#cc55cc",
+        "#7acaca",
+        "#cccccc",
+
+        "#555555",
+        "#ff5555",
+        "#55ff55",
+        "#ffff55",
+        "#5555ff",
+        "#ff55ff",
+        "#55ffff",
+        "#ffffff",
+    }
+
+    for index, color in pairs(colors) do
+        vim.g["terminal_color_" .. (index - 1)] = color
+    end
 end
 
 return { colorscheme }
