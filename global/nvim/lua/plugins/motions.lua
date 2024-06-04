0935f00d82b9ff3dc7b51ccd280feb14bf56bd1e
Alex Eager <aweager@noreply.users.github.com>
2024-06-04 14:24:30 -0700

    _ for camel case text object

--- global/nvim/lua/plugins/motions.lua
+++ global/nvim/lua/plugins/motions.lua
@@ -94,10 +94,10 @@ return {
         "bkad/CamelCaseMotion",
         config = function()
             local opts = { silent = true }
-            vim.keymap.set("o", "ic", "<Plug>CamelCaseMotion_iw", opts)
-            vim.keymap.set("x", "ic", "<Plug>CamelCaseMotion_iw", opts)
-            vim.keymap.set("o", "ac", "<Plug>CamelCaseMotion_aw", opts)
-            vim.keymap.set("x", "ac", "<Plug>CamelCaseMotion_aw", opts)
+            vim.keymap.set("o", "i_", "<Plug>CamelCaseMotion_iw", opts)
+            vim.keymap.set("x", "i_", "<Plug>CamelCaseMotion_iw", opts)
+            vim.keymap.set("o", "a_", "<Plug>CamelCaseMotion_aw", opts)
+            vim.keymap.set("x", "a_", "<Plug>CamelCaseMotion_aw", opts)
         end,
     },
 }
