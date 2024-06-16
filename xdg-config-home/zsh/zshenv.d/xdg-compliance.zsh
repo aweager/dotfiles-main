7e5821305cb384de8d9590c62e288084cfc4f229
Alex Eager <156651956+aweager@users.noreply.github.com>
2024-06-15 23:27:50 -0700

    fix reload and try to make vim xdg compliant

--- xdg-config-home/zsh/zshenv.d/xdg-compliance.zsh
+++ xdg-config-home/zsh/zshenv.d/xdg-compliance.zsh
@@ -2,3 +2,5 @@ export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 
 export TERMINFO="$XDG_DATA_HOME/terminfo"
 export TERMINFO_DIRS="$XDG_DATA_HOME/terminfo:/usr/share/terminfo"
+
+export VIMINIT='let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.lua" | so $MYVIMRC'
