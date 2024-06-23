59558e2b12e96d7f08dead373e45d5b380f7033c
Alex Eager <156651956+aweager@users.noreply.github.com>
2024-06-23 14:25:23 -0700

    beef up pretty process table printing

--- xdg-config-home/zsh/zshrc.d/aliases.zsh
+++ xdg-config-home/zsh/zshrc.d/aliases.zsh
@@ -11,6 +11,35 @@ function gd() {
 }
 
 # process hell
+zmodload zsh/zutil
 function ptable() {
-    ps -U "$USER" -o pid -o command | grep "$@" | grep -v grep
+    local -a arg_trunc
+    zparseopts -D \
+        {t,-truncate}=arg_trunc
+
+    # try to clean up command view
+    ps -U "$USER" -o pid -o command | grep "$@" | grep -v grep | awk '
+        interpreters["zsh"]=""
+        interpreters["bash"]=""
+        interpreters["python3"]=""
+        interpreters["python3.10"]=""
+        interpreters["python3.11"]=""
+        interpreters["python3.12"]=""
+        interpreters["Python"]=""
+        function basename(file, a, n) {
+            n = split(file, a, "/")
+            return a[n]
+        }
+        {
+            printf "%6s", $1
+            $2=basename($2)
+            if ($2 in interpreters) {
+                $3=basename($3)
+                for (i=3; i <= NF; i++) printf " %s", $i
+            } else {
+                for (i=2; i <= NF; i++) printf " %s", $i
+            }
+            print ""
+        }
+    ' | { if [[ -z $arg_trunc ]]; then cat; else cut -c "-$COLUMNS"; fi }
 }
