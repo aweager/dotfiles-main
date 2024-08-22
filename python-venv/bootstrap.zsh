dd584d5b599d27e867b6dcc23dca904dbbb0ec57
Alex Eager <156651956+aweager@users.noreply.github.com>
2024-08-21 21:29:07 -0700

    Support multiple python venv versions

--- python-venv/bootstrap.zsh
+++ python-venv/bootstrap.zsh
@@ -5,17 +5,29 @@ dumb clone aweager/mux-api
 dumb clone aweager/tmux-mux
 dumb clone aweager/nvim-mux
 
-if [[ ! -d "$HOME/.local/venv" ]]; then
-    if which python3.12 &> /dev/null; then
-        python3.12 -m venv "$HOME/.local/venv"
-    elif which python3.11 &> /dev/null; then
-        python3.11 -m venv "$HOME/.local/venv"
-    else
-        printf '%s\n' 'Need at least python3.11 for venv' >&2
-        return 1
+local -a supported_versions=(3.12 3.11)
+local version
+for version in "$supported_versions[@]"; do
+    if which "python${version}" &> /dev/null; then
+        if [[ ! -e "$HOME/.local/venv${version}/bin/activate" ]]; then
+            "python${version}" -m venv "$HOME/.local/venv${version}"
+        fi
+
+        source "$HOME/.local/venv${version}/bin/activate"
+        pip3 install -r "${0:a:h}/requirements.txt"
+        deactivate
+
     fi
+done
+
+for version in "$supported_versions[@]"; do
+    if [[ -e "$HOME/.local/venv${version}/bin/activate" ]]; then
+        rm "$HOME/.local/venv" &> /dev/null || true
+        ln -s "$HOME/.local/venv${version}" "$HOME/.local/venv"
+        break
+    fi
+done
+
+if [[ ! -e "$HOME/.local/venv" ]]; then
+    printf 'Python venv missing! Supported versions: %s\n' "$supported_versions" &> /dev/null
 fi
-
-source "$HOME/.local/venv/bin/activate"
-
-pip3 install -r "${0:a:h}/requirements.txt"
--- xdg-config-home/zsh/zshrc.d/venv.zsh
+++ xdg-config-home/zsh/zshrc.d/venv.zsh
@@ -1,3 +1,5 @@
-if [[ -e "$HOME/.local/venv/bin/activate" ]]; then
-    source "$HOME/.local/venv/bin/activate"
+if [[ -e "$HOME/.local/venv${AWE_VENV}/bin/activate" ]]; then
+    source "$HOME/.local/venv${AWE_VENV}/bin/activate"
+else
+    printf 'Python venv not found at %s\n' "~/.local/venv${AWE_VENV}" >&2
 fi
